a99effab1314eabf8e54feb4e44c4ed913be2a27
DYuHLin <damianver2000@gmail.com>
2023-08-17 13:44:16 +0200

    Got some responsiveness and trying to get the functionality to work on the contact page.

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ menuLinks.forEach((btn) => {
         menu.classList.toggle("active");
     });
 });
+
+const contactForm = document.getElementById("contact-form"),
+      contactName = document.getElementById("contact-name"),
+      contactEmail = document.getElementById("contact-email"),
+      contactMessage = document.getElementById("contact-message"),
+      contactCon = document.getElementById("contact-title")
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //checks if inputs have values
+    if (contactName.value === '' || contactEmail.value === '' || contactMessage.value === ''){
+
+        //show message
+        contactCon.textContent = 'Write in all the fields'
+    }else{
+        //serviceId, templateId, #form, publicKey
+        emailjs.sendForm('service_hvfhtef', 'template_cb1m6xe', '#contact-form', 'hW9sOe3e6b6pT5EwK')
+            .then(() => {
+                //show message
+                let mmg = confirm("Message sent")
+
+            }, (error) => {
+                alert('Something went wrong...', error)
+            })
+
+            //clear input after sending
+            contactName.value = ''
+            contactEmail.value = ''
+            contactMessage.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
+
